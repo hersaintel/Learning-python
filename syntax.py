0f2439cad0b01cc3c3ef453f16abd97d213ac5c8
hersaintel <gitlab.frame027@passinbox.com>
2026-06-01 01:01:12 -0400

    more syntaxing

--- syntax.py
+++ syntax.py
@@ -5,5 +5,13 @@ if 5 > 2:
 
 #Python Statements
 print("Hello World!")
-print("Have a good day.")
+print('Have a good day.')
 print("Learning Python is fun!") 
+
+#Printing text you can use single quotes '' or double quotes ""
+#printing number - doesn't require quotes
+print('I am', 35, 'years old.')
+
+#Printing text in the same line
+print('I am wealthy', end=" ")
+print(28)
