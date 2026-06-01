3c4f66e394dc330deb867f598d53da53b6c23b98
hersaintel <gitlab.frame027@passinbox.com>
2026-06-01 02:44:48 -0400

    variables

---
+++ Variables/example1.py
@@ -0,0 +1,31 @@
+x=4
+y="June"
+print(x)
+print(y)
+
+x=5       #x is of type int
+x="Sally" #x is of type str
+print(x)
+
+#Specify data type using casting
+a=str(3)  #result ('3')
+b=int(3)  #result 3
+c=float(3) #result 3.0
+
+#You can get the type of a variable using the type() function
+y=5
+z="Sally"
+c=3.0
+b=True
+print(type(y))
+print(type(z))
+print(type(c))
+print(type(b))
+
+#Case-sensitive~variable names are case sensitive
+a=4
+A="John"
+#A will not overwrite a
+
+
+
---
+++ Variables/var_names.py
@@ -0,0 +1,26 @@
+#Legal variable names
+myvar = "John"
+my_var = "John"
+_my_var = "John"
+myVar = "John"
+MYVAR = "John"
+myvar2 = "John"
+
+#Many values to multiple variables
+x,y,z="Bananas","Oranges","Apples"
+print(x)
+print(y)
+print(z)
+
+#One value to multiple variables
+x=y=z="Apples"
+print(x)
+print(y)
+print(z)
+
+#Unpacking a collection
+fruits=["Oranges","Apples","bananas"]
+x,y,z=fruits
+print(x)
+print(y)
+print(z)
