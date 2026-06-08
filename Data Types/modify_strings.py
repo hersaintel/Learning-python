0ebfef3c35062043c2d1646da06bbc092c9ffb2d
hersaintel <gitlab.frame027@passinbox.com>
2026-06-08 04:49:39 -0400

    how to modify strings

---
+++ Data Types/modify_strings.py
@@ -0,0 +1,19 @@
+#The upper() method returns the string in upper case:
+a = "Hello, World!"
+print(a.upper())
+
+#The lower() method returns the string in lower case:
+b = "Hello, World!"
+print(a.lower())
+
+#The strip() method removes any whitespace from the beginning or the end:
+c = " Hello, World! "
+print(a.strip())
+
+#The replace() method replaces a string with another string:
+a = "Hello, World!"
+print(a.replace("H", "J"))
+
+#The split() method returns alist where the the string into substrings if it finds instances of the separator:
+a = "Hello, World"
+print(a.split(","))
