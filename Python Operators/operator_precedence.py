d9b31c2b551eb0730e1bf908c3f3f8acf5a63793
hersaintel <gitlab.frame027@passinbox.com>
2026-06-16 05:17:19 -0400

    Bitwise & Precedence Operators

---
+++ Python Operators/operator_precedence.py
@@ -0,0 +1,6 @@
+#Operator precedence describes the order in which operations are performed
+#Parentheses has the highest precedence, meaning that expressions inside parenteses must be evaluated first:
+print((6 + 3) - (6 +3))
+
+#Multiplication * has higher precedence than addition +, and therefore multiplications are evaluated before additions:
+print(100 + 5 * 3) 
