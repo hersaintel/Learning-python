40549ea83e1030669724ff0f4e97aede54a7cea1
hersaintel <gitlab.frame027@passinbox.com>
2026-06-19 14:51:50 -0400

    More Change list Items

--- Python Lists/change_list.py
+++ Python Lists/change_list.py
@@ -3,3 +3,28 @@
 thislist = ["apple", "banana", "cherry"]
 thislist[1] = "blackcurrant"
 print(thislist)
+
+#To change the value of items within a specific range, define a list with the new values, and refer to the range of index numbers where you want to insert the new values:
+#Change the values "banana" and "cherry" with the values "blackcurrant" and "watermelon":
+thislist = ["apple", "banana", "cherry", "orange", "kiwi", "mango"]
+thislist[1:3] = ["blackcurrant", "watermelon"]
+print(thislist)
+
+#If you insert more items than you replace, the new items will be inserted where you specified, and the remaining items will move accordingly:
+#Change the second value by replacing it with two new values:
+thislist = ["apple", "banana", "cherry"]
+thislist[1:2] = ["blackcurrant", "watermelon"]
+print(thislist) 
+
+#If you insert less items than you replace, the new items will be inserted where you specified, and the remaining items will move accordingly:
+#Change the second and third value by replacing it with one value:
+thislist = ["apple", "banana", "cherry"]
+thislist[1:3] = ["watermelon"]
+print(thislist) 
+
+#To insert a new list item, without replacing any of the existing values use the insert() method. The insert() method inserts an item at the specified index
+
+#Insert "watermelon" as the third item:
+thislist = ["apple", "banana", "cherry"]
+thislist.insert(2, "watermelon")
+print(thislist)
