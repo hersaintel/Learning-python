db5af897a3115b5f1e8bcaabe19d03403e1fac75
hersaintel <gitlab.frame027@passinbox.com>
2026-06-25 14:30:24 -0400

    Sort lists

---
+++ Python Lists/sortlists.py
@@ -0,0 +1,21 @@
+#List objects have a sort() method that will sort the list alphanumerically, ascending, by default:
+
+thislist = ["apple","mango", "banana", "cherry"]
+thislist.sort()
+print(thislist)
+
+#Sort the list numerically
+thislist = [23,85, 1, 17, 75, 48]
+thislist.sort()
+print(thislist)
+
+#Sort List objects in descending order
+
+thislist = ["apple","mango", "banana", "cherry"]
+thislist.sort(reverse = True)
+print(thislist)
+
+#Sort the list in descending order
+thislist = [23,85, 1, 17, 75, 48]
+thislist.sort(reverse = True)
+print(thislist)
