30bd763d8c5d0853481893c2f06c992262461de4
hersaintel <gitlab.frame027@passinbox.com>
2026-07-06 15:24:58 -0400

    Access Sets

---
+++ Python Sets/accessSet.py
@@ -0,0 +1,12 @@
+#You can access set items by looping through the items using the for loop or in keyword
+thisset = {"apple", "banana", "cherry"}
+for x in thisset:
+    print(x)
+
+#Check if a set item is available in a given set:
+thisset = {"apple", "banana", "cherry"}
+print("apple" in thisset)
+
+#Check if an item is not in a given set
+thisset = {"apple", "banana","cherry"}
+print("kiwi" not in thisset)
