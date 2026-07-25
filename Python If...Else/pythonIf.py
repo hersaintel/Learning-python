9fdfd2762f5e8494b35d4fdc702a09210c2bb4c1
hersaintel <gitlab.frame027@passinbox.com>
2026-07-25 01:46:05 -0400

    Python If

---
+++ Python If...Else/pythonIf.py
@@ -0,0 +1,28 @@
+"""
+Python supports the usual logical conditions from mathematics:
+
+    Equals: a == b
+    Not Equals: a != b
+    Less than: a < b
+    Less than or equal to: a <= b
+    Greater than: a > b
+    Greater than or equal to: a >= b
+"""
+#If statement:
+a = 33
+b = 200
+if b > a:
+    print("b is greater than a)
+
+#Multiple statements in an if block:
+age = 20
+if age >= 18:
+    print("You are an adult")
+    print("You can vote")
+    print("You can drink")
+    print("You have full legal rights")
+
+#Using a Boolean variable:
+is_logged_in = True
+if is_logged_in:
+    print("Welcome back!")
